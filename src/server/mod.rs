5cb9cdeaaa64dd9d7becba660fadfe8cf0fcad81
Joe Corcoran <joe@corcoran.io>
2016-08-21 14:41:36 +0200

    Get handler

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,5 @@
 use hyper::server::{Server, Request, Response};
+use hyper::status::StatusCode;
 use reroute::{Captures, Router};
 use store::Store;
 
@@ -9,20 +10,29 @@ pub struct ServerOptions<'a> {
 
 pub fn start(db: String, options: ServerOptions) {
     let mut router = Router::new();
-
     let store = Store::shared(db);
 
     let get_store = store.clone();
-    router.get(r"/keys/([^/]+)$", move |_, res, captures| {
+    router.get(r"/keys/([^/]+)$", move |_: Request, mut res: Response, cap: Captures| {
         let s = get_store.lock().unwrap();
-        println!("{:?}", captures.unwrap());
-        res.send(b"hi");
+        if let Some(key) = cap.unwrap().pop() {
+            match s.read(key) {
+                Some(value) => {
+                    *res.status_mut() = StatusCode::Ok;
+                    res.send(value.as_bytes()).unwrap();
+                },
+                None => {
+                    *res.status_mut() = StatusCode::NotFound;
+                    res.send(b"Not found").unwrap();
+                }
+            };
+        }
     });
 
     let post_store = store.clone();
     router.post("/keys", move |_, res, _| {
         let s = post_store.lock().unwrap();    
-        res.send(b"hi");
+        res.send(b"hi").unwrap();
     });
 
     router.finalize().unwrap();
